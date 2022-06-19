06e588a635ace5865879a172b8dd162654266b9e
FishWithoutPants <fish_without_pants@mail.ru>
2022-06-19 15:59:48 +0300

    NoSuchElementException handling added

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
@@ -6,6 +7,17 @@ class BasePage():
         self.browser = browser
         self.url = url
 
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
     def open(self):
         self.browser.get(self.url)
 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
